79d1f4f456178514b533ed8ccb17e6519a9e3d36
GeorgeH <default_hyn@163.com>
2023-03-27 10:54:22 +0800

    update config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -118,7 +118,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: '/img/logo.png', // 导航栏logo
+    logo: '/img/avatar.png', // 导航栏logo
     repo: 'GeorgeHcc/GeorgeHcc.github.io', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
@@ -173,7 +173,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     // 博主信息 (显示在首页侧边栏)
     blogger: {
       // avatar: 'https://fastly.jsdelivr.net/gh/xugaoyi/image_store/blog/20200103123203.jpg',
-      avatar:'./public/img/avatar.png',
+      avatar:'/img/avatar.png',
       name: 'GeorgeH',
       slogan: 'talk is cheap,show me the code',
     },
